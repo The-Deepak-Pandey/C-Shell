254ec21ce4e608ef0630d11d823da9024788faab
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2025-07-06 10:56:11 +0530

    fixed flag issue

--- change_directrix.c
+++ change_directrix.c
@@ -77,42 +77,6 @@ void hop_handler(char *pipe, int flag_read, char *in_file)
     }
     if (flag_read)
     {
-        // FILE *file;
-        // char line[COMMAND_PATH];
-        // file = fopen(in_file, "r");
-        // if (file == NULL)
-        // {
-        //     perror("Error opening file");
-        //     return;
-        // }
-        // else
-        // {
-        //     while (fgets(line, sizeof(line), file))
-        //     {
-        //         if (line[strlen(line) - 1] == '\n')
-        //         {
-        //             line[strlen(line) - 1] = '\0';
-        //         }
-        //         else
-        //         {
-        //             line[strlen(line)] = '\0';
-        //         }
-        //         char *line2 = strtok(line, " \t");
-        //         while (line2 != NULL)
-        //         {
-        //             char previous_dir2[PATH_MAX];
-        //             getcwd(previous_dir2, PATH_MAX);
-        //             if (hop(line2))
-        //             {
-        //                 char prev3[PATH_MAX];
-        //                 getcwd(prev3, PATH_MAX);
-        //                 if (strcmp(prev3, previous_dir2) != 0)
-        //                     strcpy(previous_dir, previous_dir2);
-        //             }
-        //             line2 = strtok(NULL, " \t");
-        //         }
-        //     }
-        // }
         printf("Invalid use of redirection!!\n");
     }
 }
